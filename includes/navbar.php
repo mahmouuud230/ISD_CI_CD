115d606de8461be5b7c98c5855860c71672b8827
mahmouuud230 <168327088+mahmouuud230@users.noreply.github.com>
2024-12-01 15:20:39 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-static-top">
     <div class="container">
       <div class="navbar-header">
-        <a href="https://www.youtube.com/channel/UCsFgC9ggwrmYR2XqEHXpbNg?view_as=subscriber" class="navbar-brand"><b>Ser</b>Bermz</a>
+        <a href="https://www.youtube.com/channel/UCsFgC9ggwrmYR2XqEHXpbNg?view_as=subscriber" class="navbar-brand"><b>Lib</b>rary</a>
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
           <i class="fa fa-bars"></i>
         </button>
